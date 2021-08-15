a6c4ed0117e900484bc11ae9f736c40674711659
HellenNamulinda <hellennamulinda@gmail.com>
2021-08-15 17:32:53 +0300

    file structure

--- script.js
+++ script.js
@@ -1,12 +1,31 @@
-function convertToRoman () {
-  num = document.getElementById ('number').value;
-  if (num.length == 0 || isNaN (num)) {
-    alert ('Please enter a valid number to convert');
+function palindrome() {
+  str = document.getElementById("palindrome").value;
+  //remove all non-alphanumeric characters and converting to lowercase
+  str1 = str.replace(/[^0-9a-z]/gi, "").toLowerCase();
+  //convert string into an array
+  let strArray = str1.split("");
+  //reverse the array
+  let reversedArray = strArray.reverse();
+  //join the reversed array back to string
+  let reversedStr = reversedArray.join("");
+
+  //comparing the strings
+  if (reversedStr == str1) {
+    alert("It is a Palindrome");
+  } else {
+    alert("it is not a Palindrome");
+  }
+}
+
+function convertToRoman() {
+  num = document.getElementById("number").value;
+  if (num.length == 0 || isNaN(num)) {
+    alert("Please enter a valid number to convert");
     return false;
   } else if (num <= 0) {
     alert("Please enter a positive number");
     return false;
-  } else if (num >1000) {
+  } else if (num > 1000) {
     alert("Please enter upto 1000");
     return false;
   } else {
@@ -48,9 +67,10 @@ function convertToRoman () {
     while (i--) roman_num = (key[+digits.pop() + i * 10] || "") + roman_num;
     romamNumber = Array(+digits.join("") + 1).join("M") + roman_num;
 
-     let div = document.createElement("div");
-     div.className = "number";
-     div.innerHTML = num + " in roman numerals is : " + "<b>" + romamNumber + "</b>";
-     document.body.appendChild(div); 
+    let div = document.createElement("div");
+    div.className = "number";
+    div.innerHTML =
+      num + " in roman numerals is : " + "<b>" + romamNumber + "</b>";
+    document.body.appendChild(div);
   }
 }
